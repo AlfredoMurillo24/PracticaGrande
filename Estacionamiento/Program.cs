6a79916a623b518e24e81599ead0dc71a7ca005f
ANTRAXMUSA <ANTRAXMUSA@LAPTOP-O82C1MS8>
2022-09-29 09:53:57 -0700

    Add project files.

---
+++ Estacionamiento/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamiento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double min;
+
+            Console.WriteLine("Dame los minutos:");
+            min = Convert.ToDouble(Console.ReadLine());
+
+            if (min <= 60)
+            {
+                Console.WriteLine("Pagas $5 pesos");
+                Console.ReadLine();
+
+            }
+            if (min >= 61 & min <= 120)
+            {
+                Console.WriteLine("Pagas $15 pesos");
+                Console.ReadLine();
+
+            }
+            if (min > 121)
+            {
+                Console.WriteLine("Pagas $40 pesos");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ parimpar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parimpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double numero;
+
+            Console.WriteLine("Dame el numero y te dire si es par o impar");
+            numero = Convert.ToDouble(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero es par");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("El numero es impar");
+                Console.ReadLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
